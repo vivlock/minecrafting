2ac10ed5e6cbb6eb46a4e37a490fda2db251d4c4
Sarah Johnston <sarah.johnston@equipmentshare.com>
2019-09-30 15:32:28 -0500

    clean up digcylinder formatting

--- digCylinder.lua
+++ digCylinder.lua
@@ -6,8 +6,8 @@ radius = 15
 height = 10
  
 if tonumber(args[1]) ~= nil and tonumber(args[2]) ~= nil then
-        radius = args[1]
-        height = args[2]
+  radius = args[1]
+  height = args[2]
 end
  
 xCoord = 0
@@ -23,139 +23,130 @@ orientations = {"north", "east", "south", "west"}
 zDiff = {-1, 0, 1, 0}
 xDiff = {0, 1, 0, -1}
  
- 
+-- wait what but why
 function left()
-        orientation = orientation - 1
-        orientation = (orientation - 1) % 4
-        orientation = orientation + 1
-        turtle.turnLeft()
+  orientation = orientation - 1
+  orientation = (orientation - 1) % 4
+  orientation = orientation + 1
+
+  turtle.turnLeft()
 end
- 
- 
- 
+
 function right()
-        orientation = orientation - 1
-        orientation = (orientation + 1) % 4
-        orientation = orientation + 1
- 
-        turtle.turnRight()
+  orientation = orientation - 1
+  orientation = (orientation + 1) % 4
+  orientation = orientation + 1
+
+  turtle.turnRight()
 end
- 
- 
- 
+
 function moveForward()
-        xCoord = xCoord + xDiff[orientation]  
-        zCoord = zCoord + zDiff[orientation]
- 
-        moved = false
-        while not(moved) do
-                turtle.dig()
-                moved = turtle.forward()
-        end
+  xCoord = xCoord + xDiff[orientation]  
+  zCoord = zCoord + zDiff[orientation]
+
+  moved = false
+  while not(moved) do
+    turtle.dig()
+    moved = turtle.forward()
+  end
 end
- 
- 
- 
+
 function moveUp()
-        yCoord = yCoord + 1
- 
-        turtle.digUp()
- 
-        moved = false
-        while not(moved) do
-                moved = turtle.up()
-        end  
+  yCoord = yCoord + 1
+
+  turtle.digUp()
+
+  moved = false
+  while not(moved) do
+    moved = turtle.up()
+  end  
 end
  
 function moveDown()
- 
-        yCoord = yCoord - 1
-        turtle.digDown()
-       
-        moved = false
- 
-        while not(moved) do
-                moved = turtle.down()
-        end
- 
+  yCoord = yCoord - 1
+  turtle.digDown()
+  
+  moved = false
+
+  while not(moved) do
+    moved = turtle.down()
+  end
 end
  
 function look(direction)
-        while direction ~= orientations[orientation] do
-                right()
-        end
+  -- while [direction arg] does not equal current orientation, turn right
+  -- thanks i hate it
+  while direction ~= orientations[orientation] do
+    right()
+  end
 end
  
  
 function round(n)
-        decimal = n - math.floor(n)
-        if decimal >= 0.5 then
-                return math.ceil(n)
-        else
-                return math.floor(n)
-        end
+  decimal = n - math.floor(n)
+  if decimal >= 0.5 then
+    return math.ceil(n)
+  else
+    return math.floor(n)
+  end
 end
  
 function withinRadius(x, z)
-        return round( math.sqrt((x * x) + (z * z)) ) <= radius
+  return round( math.sqrt((x * x) + (z * z)) ) <= radius
 end
  
  
 function forwardWithinRadius()
- 
-        xNext = xCoord + xDiff[orientation]  
-        zNext = zCoord + zDiff[orientation]
-       
-        return withinRadius(xNext, zNext)
+  xNext = xCoord + xDiff[orientation]  
+  zNext = zCoord + zDiff[orientation]
+  
+  return withinRadius(xNext, zNext)
 end
  
 function gotoEdge()
-        while forwardWithinRadius() do
-                moveForward()
-        end
-       
-        left()
-       
-        xStartRing = xCoord
-        zStartRing = zCoord
+  while forwardWithinRadius() do
+    moveForward()
+  end
+  
+  left()
+  
+  xStartRing = xCoord
+  zStartRing = zCoord
 end
  
 function digRing()
- 
-        repeat
-               
-                while forwardWithinRadius() do
-                        moveForward()
-                end
-                while not(forwardWithinRadius()) do
-                        left()
-                end
-                moveForward()
-                right()
-       
-        until xCoord == xStartRing and zCoord == zStartRing
-       
-        left()
-        left()
-        for i = 1,radius do
-                moveForward()
-        end
-        left()
-        left()
+  repeat 
+    while forwardWithinRadius() do
+            moveForward()
+    end
+    while not(forwardWithinRadius()) do
+            left()
+    end
+    moveForward()
+    right()
+  until xCoord == xStartRing and zCoord == zStartRing
+  -- this is extremely inelegant and I hate it
+  left()
+  left()
+  for i = 1,radius do
+    moveForward()
+  end
+  left()
+  left()
 end
  
 function digLayer()
-        r = radius
-        for i = 1,r do
-                radius = i
-                gotoEdge()
-                digRing()
-        end
+  r = radius
+  for i = 1,r do
+    radius = i
+    gotoEdge()
+    digRing()
+  end
 end
  
 function digCylinder()
-        for i = 1,height do
-                digLayer()
-                moveDown()
-        end
+  for i = 1,height do
+    digLayer()
+    moveDown()
+  end
 end
